873f64d3e40832dca791131d6746477eff53eecc
mretamos0 <maxiretamosotrotta@gmail.com>
2024-11-05 01:49:28 -0300

    first commit

--- TP 5/punto 3/main.py
+++ TP 5/punto 3/main.py
@@ -2,7 +2,7 @@ import re
 from procesar_superheroes_villanos import Personaje
 
 class Main():
-    personajes = Personaje('TP 5\\Punto3\\superheroes_villanos.txt')
+    personajes = Personaje('TP 5\\Punto 3\\superheroes_villanos.txt')
     personajes.procesar_archivo()
     personajes.mostrar_resultados()
 
