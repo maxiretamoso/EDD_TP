7207c124c01a6626dbe2862e0f850e13a498a5a2
mretamos0 <maxiretamosotrotta@gmail.com>
2024-11-08 15:23:43 -0300

    first commit

--- TP 1/Ejercicio_1.py
+++ TP 1/Ejercicio_1.py
@@ -1,14 +1,19 @@
-# Solicite al usuario el ingreso de un número entero entre 1 y 20
-def tablas_de_multiplicar():
-    numero = int(input("Ingrese un número entero entre 1 y 20:"))
+def tabla_multiplicar():
+    while True:
+        try:
+            num = int(input("Ingrese un número entre 1 y 20: "))
 
-    if 1 <= numero <= 20:
-        print("tabla de multiplicacion del 1 al 10 de", numero)
-        # Muestre por pantalla las tablas de multiplicación del 1 al 10 
-        for i in range(1, 11):
-            tabla = numero * i
-            print(numero, "*", i, "=", tabla)   
-    else:
-        print("Ingreso invalido. No es un entero entre 1 y 20")  
+            if 1 <= num <= 20:
+                print(f'\nTabla de multiplicar de {num} del 1 al 10:')
+                
+                for i in range(1, 11):
+                    print(f'{num} * {i} = {num * i}')
+                break  
 
-tablas_de_multiplicar() 
+            print("El número ingresado no está entre 1 y 20. Vuelva a intentarlo.")
+        
+        except ValueError:
+            print("No ha ingresado un número entero. Vuelva a intentarlo.")
+
+
+tabla_multiplicar()
