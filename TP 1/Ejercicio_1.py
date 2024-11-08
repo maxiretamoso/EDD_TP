d0ba1a5670499357c6889e1936beb8b1bc792e2d
mretamos0 <maxiretamosotrotta@gmail.com>
2024-11-08 15:27:13 -0300

    first commit

--- TP 1/Ejercicio_1.py
+++ TP 1/Ejercicio_1.py
@@ -5,12 +5,11 @@ def tabla_multiplicar():
 
             if 1 <= num <= 20:
                 print(f'\nTabla de multiplicar de {num} del 1 al 10:')
-                
                 for i in range(1, 11):
                     print(f'{num} * {i} = {num * i}')
                 break 
-            
             print("El número ingresado no está entre 1 y 20. Vuelva a intentarlo.")
+            
         except ValueError:
             print("No ha ingresado un número entero. Vuelva a intentarlo.")
 
