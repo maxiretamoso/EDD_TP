8273b1b59e82b798356a120653830a6bf2794413
mretamos0 <maxiretamosotrotta@gmail.com>
2024-11-09 14:22:38 -0300

    first commit

--- TP 1/Ejercicio_4.py
+++ TP 1/Ejercicio_4.py
@@ -1,13 +1,17 @@
 def secuencia(num):
+    if not num:  
+        print("\nLa secuencia esta vacia, no se pueden realizar operaciones")
+        return
+    
     suma = sum(num)
     prom = suma / len(num)
     dif = len(num) == len(set(num))
     
-    print(f'\nSecuencia de numeros: {num}')
+    print(f'\nSecuencia: {num}')
     print(f'La sumatoria es: {suma}')
     print(f'El promedio es: {prom}')
     print(f'¿Los numeros son diferentes? {dif}')
 
 
-num = (5,6,4)
+num = [5,6,4]
 secuencia(num)
