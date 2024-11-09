00eb87e6084cd0620338a5640c5a0441e548691b
mretamos0 <maxiretamosotrotta@gmail.com>
2024-11-09 16:54:48 -0300

    first commit

--- TP 1/Ejercicio_4.py
+++ TP 1/Ejercicio_4.py
@@ -1,8 +1,22 @@
 def secuencia(num):
     if not num:  
-        print("\nLa secuencia esta vacia, no se pueden realizar operaciones")
-        return
-    
+        print("\nLa secuencia esta vacia")
+        while True:
+            try:
+                n = int(input("¿Cuantos numeros va a usar para armar la secuencia? "))
+                break
+            except ValueError:
+                print("No ha ingresado un numero entero, vuelva a ingresar")
+        
+        for i in range(n):
+            while True: 
+                try:
+                    numeros = int(input(f'Ingrese el numero {i+1}/{n}: '))
+                    num.append(numeros)
+                    break
+                except ValueError:
+                    print("No ha ingresado un numero entero, vuelva a ingresar")
+
     suma = sum(num)
     prom = suma / len(num)
     dif = len(num) == len(set(num))
@@ -10,8 +24,7 @@ def secuencia(num):
     print(f'\nSecuencia: {num}')
     print(f'La sumatoria es: {suma}')
     print(f'El promedio es: {prom}')
-    print(f'¿Los numeros son diferentes? {dif}')
+    print(f'¿Todos los numeros son diferentes? {dif}')
 
-
-num = [5,6,4]
+num = []
 secuencia(num)
