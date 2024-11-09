82815ee03d9bb62968962ee61f9865807fce53bb
mretamos0 <maxiretamosotrotta@gmail.com>
2024-11-09 03:19:28 -0300

    first commit

--- TP 1/Ejercicio_3.py
+++ TP 1/Ejercicio_3.py
@@ -1,46 +1,68 @@
-# Escriba un programa que exija al usuario el ingreso de:
-# a) 2 números enteros y determine el mayor.
 def mayor_entre_dos():
-    print("Ingrese 2 numeros enteros y se determinara el mayor")
-    num1 = int(input("Ingrese el primer numero:"))
-    num2 = int(input("Ingrese el segundo numero:"))
-    
-    if num1 > num2:
-        print(num1, "Es mayor que", num2)
-    elif num1 < num2:
-        print(num2,"Es mayor que", num1)
-    else:
-        print("Ambos numeros son iguales")
-    
-# b) 3 números enteros y determine el mayor.
-def mayor_entre_tres():
-    print("Ingrese 3 numeros enteros y se determinara el mayor")
-    n1 = int(input("Ingrese el primer numero:"))
-    n2 = int(input("Ingrese el segundo numero:"))
-    n3 = int(input("Ingrese el tercer numero:"))
-    
-    if n1 > n2 and n1 > n3:
-        print(n1, "Es mayor que", n2, "y mayor que", n3)
-    elif n2 > n1 and n2 > n3:
-        print(n2, "Es mayor que", n1, "y mayor que", n3)
-    elif n3 > n1 and n3 > n2:
-        print(n3, "Es mayor que", n1, "y mayor que", n2)
-    else:
-        print("No hay un numero mayor")
-        
-# c) N números enteros y determine el mayor (no usar colecciones)
-def mayor_entre_n():
-    print("Ingrese N numeros enteros y se determinara el mayor")
-    n = int(input("Ingrese la cantidad de numeros:"))
-    mayor = 0
+    print("\nCalcular el numero mayor entre 2 enteros")
+    while True:
+        try:
+            n1 = int(input("Ingrese el primer numero entero: "))
+            n2 = int(input("Ingrese el segundo numero entero: "))
+            break
+        except ValueError:
+            print("El numero ingresado no es un entero, vuelva a ingresar")
 
+    if n1 > n2:
+        print(f'{n1} es mayor que {n2}')
+    elif n1 < n2:
+        print(f'{n2} es mayor que {n1}')
+    else:
+        print("Los numeros son iguales")
+
+def mayor_entre_tres():
+    print("\nCalcular el numero mayor entre 3 enteros")
+    while True:
+        try:
+            num1 = int(input("Ingrese el primer numero entero: "))
+            num2 = int(input("Ingrese el segundo numero entero: "))
+            num3 = int(input("Ingrese el tercer numero entero: "))
+            break
+        except ValueError:
+            print("El numero ingresado no es un entero, vuelva a ingresar")
+
+    if num2 < num1 > num3:
+        print(f'{num1} es mayor que {num2} y {num3}')
+    elif num1 < num2 > num3:
+        print(f'{num2} es mayor que {num1} y {num3}')
+    elif num1 < num3 > num2:
+        print(f'{num3} es mayor que {num1} y {num2}')
+    elif num1 & num2 > num3:
+        print(f'{num1} y {num2} son iguales y son mayores que {num3}')
+    elif num1 & num3 > num2:
+        print(f'{num1} y {num3} son iguales y son mayores que {num2}')
+    elif num2 & num3 > num1:
+        print(f'{num2} y {num3} son iguales y son mayores que {num1}')
+    else:
+        print("Los numeros son iguales")
+
+def mayor_entre_n():
+    print("\nCalcular el numero mayor entre n enteros")
+    while True:
+        try:
+            n = int(input("Ingrese la cantidad de numeros que va a usar: "))
+            break
+        except ValueError:
+            print("El numero ingresado no es un entero, vuelva a ingresar")
+
+    mayor = 0
     for i in range(n):
-        num = int(input(f"Ingrese el numero {i+1}: "))
-        if num > mayor:
-            mayor = num
-            
-    print("El numero mayor es:", mayor)
-    
+        while True:
+            try:
+                num = int(input(f'Ingrese el numero {i+1}/{n}: '))
+                if mayor < num:
+                    mayor = num
+                print(f'El numero mayor es: {mayor}')
+                break
+            except ValueError:
+                print("El numero ingresado no es un entero, vuelva a ingresar")
+          
+
 mayor_entre_dos()
 mayor_entre_tres()
 mayor_entre_n()
