45d4d83b6fd06d4233ec60e185a182b86f8d2858
mretamos0 <maxiretamosotrotta@gmail.com>
2024-11-10 17:14:39 -0300

    first commit

--- TP 1/Ejercicio_6.py
+++ TP 1/Ejercicio_6.py
@@ -1,31 +1,43 @@
-#Escriba un programa Python que solicite al usuario el nombre de cada país del continente, su nombre, superficie y cantidad de habitantes
 def componentes_paises():
-    tot_hab, tot_sup = 0, 0 # Inicializo en 0 los acumuladores del total de habitantes y superficie
+    tot_hab, tot_sup = 0, 0
     paises = []
+
+    while True:
+        try:
+            can_paises = int(input("\nIngrese la cantidad de países: "))
+            break
+        except ValueError:
+            print("\nLa cantidad de paises debe ser un numero entero, vuelva a intentarlo")
     
-    can_paises = int(input("Ingrese la cantidad de países: "))
-    
-    # Se solicita el nombre de cada país, su superficie y cantidad de habitantes:
     for i in range(can_paises):
-        pais = input(f"Ingrese el nombre del país:")
-        sup = float(input(f"Ingrese la superficie de {pais}:"))
-        hab = int(input(f"Ingrese la cantidad de habitantes de {pais}:"))
-        
+        pais = input(f'Ingrese el nombre del pais {i+1}/{can_paises}: ')
+        while True:
+            try:
+                sup = float(input(f'Ingrese la superficie de {pais}: '))
+                break
+            except ValueError:
+                print("\nLa superficie debe ser un numero, vuelva a ingresar")
+        while True:
+            try:
+                hab = int(input(f'Ingrese la cantidad de habitantes de {pais}: '))
+                break
+            except ValueError:
+                print("\nLa cantidad de habitantes debe ser un numero entero, vuelva a ingresar")
         paises.append({"pais": pais, "superficie": sup, "habitantes": hab})
-        
-    # Calcular el total de habitantes, total de superficie, y la densidad:
+
     for pais in paises:
         tot_hab += pais["habitantes"]
         tot_sup += pais["superficie"]
-        pais["densidad"] = pais["habitantes"] / pais["superficie"]
-
-    # promedio de habitantes:
+        densidad = pais["habitantes"] / pais["superficie"]
     prom_hab = tot_hab / can_paises
 
-    print("El total de habitantes es:", tot_hab)
-    print("La superficie total es:", tot_sup)
-    print("El promedio de habitantes es:", prom_hab)
     for pais in paises:
-        print(f"La densidad poblacional de {pais["pais"]} es: {pais["densidad"]}")
+        print(f'\n{pais}')
+    print(f'\nEl total de habitantes es: {tot_hab}')
+    print(f'\nLa superficie total es: {tot_sup}')
+    print(f'\nEl promedio de habitantes es: {prom_hab}')
+    for pais in paises:
+        print(f'\nLa densidad poblacional de {pais["pais"]} es: {densidad}')
+
         
 componentes_paises()
